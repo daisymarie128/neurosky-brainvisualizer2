6270843f33a73de0c0dd9ca29ad4e98a26e73e42
Daisy Smith <daisy.m.s@hotmail.com>
2015-03-11 13:36:44 +1100

    vis

--- client/visualizer.js
+++ client/visualizer.js
@@ -36,105 +36,249 @@ require(
 		brainData = datatest
 	});
 
-	console.log('data', brainData.eSense);
+	// console.log('data', brainData.eSense);
 
 	$('body').append('<div id="screen"></div>');
 
-	var t = -1;
-	var z = -5;
-    var n = 300;
-    var v = 0;
-    var data = d3.range(n).map(test);
+    // var data = d3.range(n).map(test);
 
-    function test(){
-    	if(brainData.eSense != undefined){
-	    	return {
-		    	attention: brainData.eSense.attention,
-		    	time: ++t
-	    	}
-	    } else {
-	    	return {
-		    	attention: 0,
-		    	time: ++t
-	    	}
-	    }
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 1200 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var now = new Date;
+var interval = 1000;
+
+var color = d3.scale.category20();
+
+// god damnit
+var data = [
+            [
+             {date:now, value: 0, id:"delta"}
+            ],
+            [
+              {date:now, value: 0, id:"theta"}
+            ],
+            [
+             {date:now, value: 0, id:"lowAlpha"}
+            ],
+            [
+             {date:now, value: 0, id:"highAlpha"}
+            ],
+            [
+             {date:now, value: 0, id:"lowBeta"}
+            ],
+            [
+             {date:now, value: 0, id:"highBeta"}
+            ],
+            [
+             {date:now, value: 0, id:"lowGamma"}
+            ],
+            [
+             {date:now, value: 0, id:"highGamma"}
+            ]
+           ];
+
+
+    // [0,1,2,3,4,5,6,7].map(function(index) { 
+    //     color.domain(d3.keys(data[0]).filter(function(key) { 
+    //         return key == "id" 
+    //     })
+    //     )
+    // })
+
+// add ranges
+var x = d3.time.scale().range([0, width]);
+var y = d3.scale.linear().range([height, 0]);
+
+// add default scale of the axes
+x.domain([new Date(+(now)-(10*1000)), new Date(+(now)+(4*1000))]);
+y.domain([0, 1]);
+
+var xAxis = d3.svg.axis().scale(x)
+    // add ticks (axis and vertical line)
+    .tickSize(-height).tickPadding(6).ticks(5).orient("bottom");
+
+var yAxis = d3.svg.axis().scale(y)
+    // add ticks (axis and vertical line)
+    .tickSize(-width).tickPadding(6).ticks(5).orient("left");
+
+var line = d3.svg.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.value); });
+
+var zoom = d3.behavior.zoom().x(x)
+            .scaleExtent([0.005, 5]) // allow zooming in/out
+            .on("zoom", draw);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .call(zoom);
+
+// needed for the transition
+svg.append("defs").append("clipPath")
+.attr("id", "clip")
+.append("rect")
+.attr("width", width)
+.attr("height", height);
+
+// needed for zooming and dragging
+var rect = svg.append("rect").attr("width", width).attr("height", height);
+
+// avoid data lines to overlap with axis
+var svgBox = svg.append("svg").attr("width", width).attr("height", height)
+                .attr("viewBox", "0 0 " + width + " " + height);
+
+var lines = svgBox.selectAll("g").data(data);
+
+//for each array, create a 'g' line container
+var aLineContainer = lines.enter().append("g");
+aLineContainer.append("path")
+    .attr("class", "line")
+    .style("stroke", function(d) { return color(d.key); });
+
+
+// add x axis to chart
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")");
+
+// add y axis to chart
+svg.append("g")
+    .attr("class", "y axis")
+  .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Strenthg");
+
+// show scatter points and tooltips. so far its only on first point
+var formatTime = d3.time.format("%e %B");
+var div = d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
+aLineContainer.selectAll(".dot")
+.data( function(d, i) { return d; } )  // Our nested data we call, its grouped funny im sorry
+.enter()
+  .append("circle")
+  .attr("class", "dot")
+  .attr("r", 3.5)
+  .on("mouseover", function(d) {
+            div.transition()        
+               .duration(100)      
+               .style("opacity", .9);      
+            div.html(formatTime(d.date) + "<br/>"  + d.value + "<br/>"  + d.id)  
+               .style("left", (d3.event.pageX) + "px")     
+               .style("top", (d3.event.pageY - 28) + "px");    
+            })                  
+        .on("mouseout", function(d) {       
+            div.transition()        
+                .duration(500)      
+                .style("opacity", 0);   
+        });
+
+// transistion for paths selected
+svg.append("g").attr("clip-path", "url(#clip)");
+
+var currentDate = now;
+function update() {
+  console.log("updating");
+  
+  currentDate = new Date(+(currentDate)+interval);
+
+
+
+  if(brainData.eSense != undefined){
+  var newData = [
+                 {date:currentDate, value: brainData.eegPower.delta, id:"delta"},
+                 {date:currentDate, value: brainData.eegPower.theta, id:"theta"},
+                 {date:currentDate, value: brainData.eegPower.lowAlpha, id:"lowAlpha"},
+                 {date:currentDate, value: brainData.eegPower.highAlpha, id:"highAlpha"},
+                 {date:currentDate, value: brainData.eegPower.lowBeta, id:"lowBeta"},
+                 {date:currentDate, value: brainData.eegPower.highBeta, id:"highBeta"},
+                 {date:currentDate, value: brainData.eegPower.lowGamma, id:"lowGamma"},
+                 {date:currentDate, value: brainData.eegPower.highGamma, id:"highGamma"},
+                ];
+            }else{
+                var newData = [
+                 {date:currentDate, value: 0, id:"delta"},
+                 {date:currentDate, value: 0, id:"theta"},
+                 {date:currentDate, value: 0, id:"lowAlpha"},
+                 {date:currentDate, value: 0, id:"highAlpha"},
+                 {date:currentDate, value: 0, id:"lowBeta"},
+                 {date:currentDate, value: 0, id:"highBeta"},
+                 {date:currentDate, value: 0, id:"lowGamma"},
+                 {date:currentDate, value: 0, id:"highGamma"},
+                ];
+            }
+
+                // console.log('newData', newData)
+  
+  var lowestXDomain      = x.domain()[0];
+  var highestXDomain     = x.domain()[1];
+  var currentHighestDate = d3.max(data[0], function(d) { return d.date });
+  var shiftRight         = false;
+  
+  // is current highest date currently being showed?
+  if (lowestXDomain <= currentHighestDate && currentHighestDate <= highestXDomain) {
+    var newHighestDate = d3.max(newData, function(d) { return d.date });
+    // if new highest date is out of the domain, update the domain
+    if (highestXDomain < newHighestDate) {
+      shiftRight = true;
+//      svg.select("g.y.axis").transition().duration(300).ease("linear").call(yAxis);
     }
+  }
+  
+  // only perform animated transition when needed or we will have problems when dragging/zooming
+  d3.transition().ease("linear").duration((shiftRight ? interval : 1)).each(function() {
 
-    var margin = {top: 10, right: 10, bottom: 20, left: 40},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
-
-    var x = d3.scale.linear()
-        .domain([0, n - 1])
-        .range([0, width]);
-
-    var y = d3.scale.linear()
-        .domain([0, 200])
-        .range([height, 0]);
-
-    var line = d3.svg.line()
-        .x(function(d, i) { console.log("this is line d value", d.time);
-        	return x(d.time); })
-        .y(function(d, i) { return y(d.attention); });
-
-    var svg = d3.select("body").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom);
-
-    var g = svg.append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    var graph = g.append("svg")
-        .attr("width", width)
-        .attr("height", height + margin.top + margin.bottom);
-
-    var xAxis = d3.svg.axis().scale(x).orient("bottom");
-    var axis = graph.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-    g.append("g")
-        .attr("class", "y axis")
-        .call(d3.svg.axis().scale(y).orient("left"));
-
-	var path = graph.append("g")
-		.append("path")
-		.data([data])
-		.attr("class", "line")
-		.attr("d", line);
-
-    tick();
-
-    function tick() {
-        // push a new data point onto the back
-        data.push(test());
-
-        // update domain
-        x.domain([t - n, t]);
-
-        // redraw path, shift path left
-        // console.log("this is line", line.y)
-
-        path
-            .attr("d", line)
-            .attr("transform", null)
-            .transition()
-            .duration(500)
-            .ease("linear")
-            // .attr("transform", "translate(" + t - 1 + ")")
-            .each("end", tick);
-
-        // shift axis left
-        axis
-            .transition()
-            .duration(500)
-            .ease("linear")
-            .call(d3.svg.axis().scale(x).orient("bottom"));
-
-        // pop the old data point off the front
-        data.shift();
-        console.log('this is data after shift', data)
+    if (shiftRight) {
+      x.domain([new Date(+(lowestXDomain)+(interval)), newHighestDate]);
     }
+    
+    // update domains
+    y.domain([0, d3.max(data.map(function(d) { return d3.max(d, function(dm) { return dm.value; }); } )) * 1.1]);
+  //  xAxis.scale(x);
+  //  yAxis.scale(y);
+  
+    draw();
+  
+  });
+  
+  newData.forEach(function(d, i) {
+    data[i].push(d);
+  });
+
+  aLineContainer
+   .attr("d", line)
+   .attr("transform", null)
+  
+  if (shiftRight) {
+    aLineContainer.transition()
+        .ease("linear")
+        .duration(interval)
+//        .attr("transform", "translate(" + x(-1) + ")")
+  }
+}
+
+window.setInterval(function() {
+  update();
+}, interval);
+
+
+draw();
+
+function draw() {
+  svg.select("g.x.axis").call(xAxis);
+  svg.select("g.y.axis").transition().duration(300).ease("linear").call(yAxis);
+
+  svg.selectAll("path.line").attr("d", line);
+  aLineContainer.selectAll("circle.dot").attr("cx", line.x()).attr("cy", line.y());
+//  d3.select("#footer span").text("U.S. Commercial Flights, " + x.domain().map(format).join("-"));
+}
+
+
 
 });
